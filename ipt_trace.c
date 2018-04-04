23daad8fcf3d406846d36ab5320ff04d177a0106
Stefan Nagy <snagy2@vt.edu>
2018-04-04 01:53:20 -0400

    Update ipt_trace.c

--- ipt_trace.c
+++ ipt_trace.c
@@ -13,75 +13,80 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
+#include "afl-2.52b/types.h"
+#include <sys/time.h>
 
 #define DATA_PAGES 1024
 #define AUX_PAGES 16
 
-#define _HF_PERF_MAP_SZ (1024 * 512)
-#define _HF_PERF_AUX_SZ (1024 * 1024)
+#define PERF_MAP_SZ (1024 * 512)
+#define PERF_AUX_SZ (1024 * 1024)
 
-static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
-                int cpu, int group_fd, unsigned long flags)
-{
-    int ret;
-
-    ret = syscall(__NR_perf_event_open, hw_event, pid, cpu,
-                   group_fd, flags);
-    return ret;
-}
 
 int main(int argc, char **argv)
 {
+    long start, end;
+    struct timeval timecheck;
+
     struct perf_event_attr attr;
     struct perf_event_mmap_page *header;
     void *base, *data, *aux;
     int page_size = getpagesize();
 
     memset(&attr, 0, sizeof(attr));
-    attr.size = sizeof(attr);
-    attr.type = 6; // specific to this computer
-    attr.size = sizeof(struct perf_event_attr);
 
+    attr.type = 6;
+    attr.size = sizeof(attr);
+    attr.disabled = 1;
     attr.exclude_kernel = 1;
     attr.exclude_hv = 1;
-    attr.disabled = 1;
-    //attr.precise_ip = 3;
+    
+    /*
+    attr.sample_type = 0x10086;
+    //attr.config = 0x300e601; 
+    attr.sample_id_all = 1;
+    attr.config = 0x9; //???
+    attr.mmap = 1;
+    attr.mmap2 = 1;
+    attr.comm_exec = 1;
+    attr.context_switch = 1;
+    attr.task = 1;
+    // these two below crash for some reason
+    attr.inherit = 1;
+    attr.read_format = 1;
+    */
 
     int pid = getpid();
-    int perf_fd = syscall(SYS_perf_event_open, &attr, pid, -1, -1, 0);
-    if (perf_fd == -1){
-        err(EXIT_FAILURE, "syscall");
-    }
 
-    base = mmap(NULL, _HF_PERF_MAP_SZ + page_size, PROT_WRITE, MAP_SHARED, perf_fd, 0);
-    if (base == MAP_FAILED) {
-        err(EXIT_FAILURE, "mmap");
-    }
+    int perf_fd = syscall(SYS_perf_event_open, &attr, pid, -1, -1, 0);
+    if (perf_fd == -1) err(EXIT_FAILURE, "syscall");
+
+    base = mmap(NULL, PERF_MAP_SZ + page_size, PROT_WRITE, MAP_SHARED, perf_fd, 0);
+    if (base == MAP_FAILED) err(EXIT_FAILURE, "mmap");
 
     header = base;
     data = base + header->data_offset;
     header->aux_offset = header->data_offset + header->data_size;
-    header->aux_size = _HF_PERF_AUX_SZ;
+    header->aux_size = PERF_AUX_SZ;
 
-    aux = mmap(NULL, header->aux_size, PROT_READ | PROT_WRITE, MAP_SHARED, perf_fd, header->aux_offset);
-    if (aux == MAP_FAILED) {
-        err(EXIT_FAILURE, "mmap");
-    }
+    aux = mmap(NULL, header->aux_size, PROT_READ, MAP_SHARED, perf_fd, header->aux_offset);
+    if (aux == MAP_FAILED) err(EXIT_FAILURE, "mmap");
 
-    char *args[2];
-
-    // sample program to trace
-    args[0] = "/bin/ls > /dev/null";        
-    args[1] = NULL;            
+    gettimeofday(&timecheck, NULL);
+    start = (long)timecheck.tv_sec * 1000 + (long)timecheck.tv_usec / 1000;
 
     ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
     ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
 
-    execv( args[0], args );
+    system("/benchmarks/libjpeg/jpeg-9a/djpeg /benchmarks/libjpeg/.cur_input > /dev/null" );
 
     ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
 
-    printf("Done tracing \n");
+    gettimeofday(&timecheck, NULL);
+    end = (long)timecheck.tv_sec * 1000 + (long)timecheck.tv_usec / 1000;
+
+    printf("Trace time: %.3f seconds\n", (float) (end - start)/1000);
 
     close(perf_fd);
 
@@ -95,5 +100,7 @@ int main(int argc, char **argv)
     }
 
     close(perf_fd);
-
+    munmap(aux, header->aux_size);
+    munmap(base, PERF_MAP_SZ + page_size);
 }
+
